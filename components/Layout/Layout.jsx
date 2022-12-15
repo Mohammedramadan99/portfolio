bccd9462bb462ca479df5f92081668f849f87fc3
mohammedramadan99 <lifeisagame0101@gmail.com>
2022-12-15 14:38:10 +0200

    SdaD

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Navbar from '../Navbar'
 
 function Layout({ children })
@@ -5,6 +6,11 @@ function Layout({ children })
 
   return (
     <>
+      <Head>
+        <title>{"Mohammed Ramadan"}</title>
+        <meta name="description" content="portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Navbar />
       {children}
     </>
